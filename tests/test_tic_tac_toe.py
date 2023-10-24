c05d7ac5c3b306775cff6f8862da82e034dce5a1
AminePMrad <am4672@drexel.edu>
2023-10-23 20:47:02 -0400

    update - changed testing now at 87%

--- tests/test_tic_tac_toe.py
+++ tests/test_tic_tac_toe.py
@@ -13,15 +13,144 @@ def test_print_board():
     assert output.getvalue() == expected_output
 
 def test_check_winner():
-    board = [["X", "O", "X"], [" ", "X", "O"], ["O", "X", " "]]
-    assert check_winner(board, "X")
+    # Test when X wins in the first row
+    board_x_win_row = [
+        ["X", "X", "X"],
+        [" ", " ", " "],
+        [" ", " ", " "]
+    ]
+    assert check_winner(board_x_win_row, "X") is True
+# Test when X wins in the second row
+    board_x_win_row = [
+        [" ", " ", " "],
+        ["X", "X", "X"],
+        [" ", " ", " "]
+    ]
+    assert check_winner(board_x_win_row, "X") is True
+    # Test when X wins in the third row
+    board_x_win_row = [
+        [" ", " ", " "],
+        [" ", " ", " "],
+        ["X", "X", "X"]
+    ]
+    assert check_winner(board_x_win_row, "X") is True
+    
+# Test when X wins in the first col
+    board_x_win_col = [
+        ["X", " ", " "],
+        ["X", " ", " "],
+        ["X", " ", " "]
+    ]
+    assert check_winner(board_x_win_col, "X") is True
+
+    # Test when X wins in the second col
+    board_x_win_col = [
+        [" ", "X", " "],
+        [" ", "X", " "],
+        [" ", "X", " "]
+    ]
+    assert check_winner(board_x_win_col, "X") is True
+
+    # Test when X wins in the third col
+    board_x_win_col = [
+        [" ", " ", "X"],
+        [" ", " ", "X"],
+        [" ", " ", "X"]
+    ]
+    assert check_winner(board_x_win_col, "X") is True
+
+    # Test when X wins in the first diag
+    board_x_win_diag = [
+        ["X", " ", " "],
+        [" ", "X", " "],
+        [" ", " ", "X"]
+    ]
+    assert check_winner(board_x_win_diag, "X") is True
+
+    # Test when X wins in the second diag
+    board_x_win_diag = [
+        [" ", " ", "X"],
+        [" ", "X", " "],
+        ["X", " ", " "]
+    ]
+    assert check_winner(board_x_win_diag, "X") is True
+
+    # Test when O wins in the first row
+    board_O_win_row = [
+        ["O", "O", "O"],
+        [" ", " ", " "],
+        [" ", " ", " "]
+    ]
+    assert check_winner(board_O_win_row, "O") is True
+# Test when O wins in the second row
+    board_O_win_row = [
+        [" ", " ", " "],
+        ["O", "O", "O"],
+        [" ", " ", " "]
+    ]
+    assert check_winner(board_O_win_row, "o") is True
+    # Test when O wins in the third row
+    board_O_win_row = [
+        [" ", " ", " "],
+        [" ", " ", " "],
+        ["O", "O", "O"]
+    ]
+    assert check_winner(board_O_win_row, "O") is True
+    
+# Test when O wins in the first col
+    board_O_win_col = [
+        ["O", " ", " "],
+        ["O", " ", " "],
+        ["O", " ", " "]
+    ]
+    assert check_winner(board_O_win_col, "O") is True
+
+    # Test when O wins in the second col
+    board_O_win_col = [
+        [" ", "O", " "],
+        [" ", "O", " "],
+        [" ", "O", " "]
+    ]
+    assert check_winner(board_O_win_col, "O") is True
+
+    # Test when O wins in the third col
+    board_O_win_col = [
+        [" ", " ", "O"],
+        [" ", " ", "O"],
+        [" ", " ", "O"]
+    ]
+    assert check_winner(board_O_win_col, "O") is True
+
+    # Test when O wins in the first diag
+    board_O_win_diag = [
+        ["O", " ", " "],
+        [" ", "O", " "],
+        [" ", " ", "O"]
+    ]
+    assert check_winner(board_O_win_diag, "O") is True
+
+    # Test when O wins in the second diag
+    board_O_win_diag = [
+        [" ", " ", "O"],
+        [" ", "O", " "],
+        ["O", " ", " "]
+    ]
+    assert check_winner(board_O_win_diag, "O") is True
+
+    # Test when there is no winner
+    board_no_winner = [
+        ["X", "O", "X"],
+        ["X", "O", "O"],
+        ["O", "X", "X"]
+    ]
+    assert check_winner(board_no_winner, "O") is False
 
 def test_is_draw():
     board = [["X", "O", "X"], ["O", "X", "X"], ["X", "X", "O"]]
     assert is_draw(board)
 
 def test_game():
-    user_input = ["0 0", "0 1", "1 1", "1 0", "2 0"]
+    user_input = ["0", "0", "0", "1", "1", "1", "0", "2", "2", "0"]
     expected_output = "Player X's turn\nPlayer O's turn\nPlayer X's turn\nPlayer O's turn\nPlayer X's turn\nPlayer X wins!\n"
     output = StringIO()
     input_str = '\n'.join(user_input) + '\n'
